745bf936b6830510ec34c0255acda43752071e83
Qoddi App Platform <73052948+qoddiapps@users.noreply.github.com>
2025-04-29 17:26:56 -0400

    Updated by Qoddi.ai: Added 'htmlDateString' filter as Liquid filter as well as Nunjucks filter in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -155,6 +155,10 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
+  eleventyConfig.addLiquidFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
   eleventyConfig.addFilter("sitemapDateTimeString", (dateObj) => {
     const dt = DateTime.fromJSDate(dateObj, { zone: "utc" });
     if (!dt.isValid) {
